305dfd157ac63b11b3c42607d87173ea2a770941
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-03 19:45:23 +0100

    uploaded code

--- class-constructor.js
+++ class-constructor.js
@@ -3,7 +3,13 @@ class Person {
         this.name = name;
         this.age = age;
     }
+    introduce() {
+        console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
 
 }
-    const person1 = new Person("Alice", 30);
-    console.log(person1);
+    
+
+    }
+    const person1 = new Person("John", 30);
+    person1.introduce();
+    
