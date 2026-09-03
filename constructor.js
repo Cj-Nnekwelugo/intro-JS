b267d358122a1e951e8035ad2df07e699ce84a35
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-03 19:26:08 +0100

    uploaded code

---
+++ constructor.js
@@ -0,0 +1,15 @@
+function Student(name, age) {
+    this.name = name;
+    this.age = age;
+}
+Student.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
+const student1 = new Student("Alice", 20);
+student1.greet();
+const student2 = new Student("Bob", 22);
+student2.greet();
+
+
+
