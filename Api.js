26e24fb74babf3cc52bf48a456728379a202df3e
Chijioke Nnekwelugo <cjnnekwelugo@gmail.com>
2026-09-22 19:50:50 +0100

    uploaded codesy

--- Api.js
+++ Api.js
@@ -1,6 +1,19 @@
 // Fetch API
 // using promise
-fetch('https://dummyjson.com/users')
-.then(res => res.json())
-.then(console.log);
+//fetch('https://dummyjson.com/users')
+//.then(res => res.json())
+//.then(console.log);
 
+
+// using async and await
+
+async function getAllProducts() {
+    try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.log(error.mesage)
+    }
+} 
+getAllUsers();
